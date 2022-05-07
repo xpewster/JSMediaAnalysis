2ba75af2fe3eec385026d1b6d9d142591e348416
xpewster <peturpham@gmail.com>
2022-05-07 18:47:49 -0500

    hayom analysis

--- analyze.py
+++ analyze.py
@@ -8,6 +8,9 @@ from fake_useragent import UserAgent
 import nltk
 from nltk.corpus import sentiwordnet as swn
 # nltk.download('punkt')
+# nltk.download('sentiwordnet')
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
 # from PassivePySrc import PassivePy
 
 
@@ -103,12 +106,12 @@ for p in pages:
 # ANALYSIS
 
 print ("start")
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_lg')
 # passivepy = PassivePy.PassivePyAnalyzer('en_core_web_sm')
 print ("load")
 
 palestinianWB = ['palestinian', 'palestinians', 'palestine', 'arab', 'arabs', 'west bank', 'gaza']
-israeliWB = ['israeli', 'israelis', 'israel', 'jew', 'jews', 'settler', 'settlers', 'settlement', 'settlements']
+israeliWB = ['israeli', 'israelis', 'israel', 'jew', 'jews', 'settler', 'settlers', 'settlement', 'settlements', 'idf', 'i.d.f.']
 
 
 hayom_palestinian_passive = []
@@ -116,28 +119,117 @@ hayom_israeli_passive = []
 hayom_palestinian_active = []
 hayom_israeli_active = []
 
+def is_passive(sentDoc):
+    tags = [token.dep_ for token in sentDoc]
+    sents = list(sentDoc.sents)[0]
+
+    if (any(['agent' in sublist for sublist in tags]) or any(['nsubjpass' in sublist for sublist in tags])):
+        return (True, sents.root)
+
+    if (sents.root.tag_ == "VBN"):
+        return (True, sents.root)
+    for w in sents.root.children:
+        if (w.dep_ == "aux" and w.tag_ == "VBN"):
+            return (True, w)
+
+    return (False, None)
+        
+
 for page in hayom_text:
     sentences = nltk.sent_tokenize(page)
 
+    assoc = {} 
+    SUBJS = ["nsubj", "nsubjpass", "csubj", "csubjpass", "agent", "expl"]
+
     for sent in sentences:
         sentDoc = nlp(sent)
-        tags = [token.dep_ for token in sentDoc]
 
-        if (any(['agent' in sublist for sublist in tags]) or any(['nsubjpass' in sublist for sublist in tags])):
-            
+        for token in sentDoc:
+            if (token.dep_ in SUBJS):
+                if (token.text not in assoc.keys()):
+                    assoc[token.text] = set()
+
+                for w in range(token.i+1, token.head.i):
+                    assoc[token.text].add(sentDoc[w].text)
+
+                for child in token.children:
+                    assoc[token.text].add(child.text)
+
+    for sent in sentences:
+        sentDoc = nlp(sent)
+        
+        voice = is_passive(sentDoc)
+
+        palestinian = False
+        israeli = False
+        
+
+        if (voice[0]): 
             for token in sentDoc:
                 if ((token.dep_ == "nsubjpass" or token.dep_ == "csubjpass")):
                     if (token.text.lower() in palestinianWB):
-                        hayom_palestinian_passive.append(sent)
-                    elif (token.text.lower() in israeliWB):
-                        hayom_israeli_passive.append(sent)
+                        palestinian = True         
+                    if (token.text.lower() in israeliWB):
+                        israeli = True
+                    if not palestinian and not israeli:
+                        palestinian = any([w in assoc[token.text] for w in palestinianWB])
+                        israeli = any([w in assoc[token.text] for w in israeliWB])
+
+                    if palestinian or israeli:
+                        if (token.head.pos_ == "VERB"):
+                            synsets = swn.senti_synsets(token.head.text, 'v')
+                        elif (token.head.pos_ == "ADJ"):
+                            synsets = swn.senti_synsets(token.head.text, 'a')
+
+                        words = list(synsets)
+                        if len(words) > 1:
+                            w = words[0]
+                            obj = {
+                                "pos_score": w.pos_score(),
+                                "neg_score": w.neg_score(),
+                                "obj_score": w.obj_score(),
+                                "sentence": sent,
+                                "subject": token.text,
+                                "verb": token.head.text
+                            }
+                            if palestinian:
+                                hayom_palestinian_passive.append(obj)
+                            if israeli:
+                                hayom_israeli_passive.append(obj)
         else:
             for token in sentDoc:
                 if ((token.dep_ == "nsubj" or token.dep_ == "csubj")):
                     if (token.text.lower() in palestinianWB):
-                        hayom_palestinian_active.append(sent)
-                    elif (token.text.lower() in israeliWB):
-                        hayom_israeli_active.append(sent)
+                        palestinian = True         
+                    if (token.text.lower() in israeliWB):
+                        israeli = True
+                    if not palestinian and not israeli:
+                        palestinian = any([w in assoc[token.text] for w in palestinianWB])
+                        israeli = any([w in assoc[token.text] for w in israeliWB])
+
+                    if palestinian or israeli:
+                        if (token.head.pos_ == "VERB"):
+                            synsets = swn.senti_synsets(token.head.text, 'v')
+                        elif (token.head.pos_ == "ADJ"):
+                            synsets = swn.senti_synsets(token.head.text, 'a')
+
+                        words = list(synsets)
+                        if len(words) > 1:
+                            w = words[0]
+                            obj = {
+                                "pos_score": w.pos_score(),
+                                "neg_score": w.neg_score(),
+                                "obj_score": w.obj_score(),
+                                "sentence": sent,
+                                "subject": token.text,
+                                "verb": token.head.text
+                            }
+
+                        if palestinian:
+                            hayom_palestinian_active.append(obj)
+                        if israeli:
+                            hayom_israeli_active.append(obj)
+
 
 print("-----------palestinian passive:")
 for s in hayom_palestinian_passive:
@@ -151,3 +243,17 @@ for s in hayom_palestinian_active:
 print("-----------israeli active:")
 for s in hayom_israeli_active:
     print(s)
+
+print(len(hayom_palestinian_passive)/len(hayom_palestinian_active))
+print(len(hayom_israeli_passive)/len(hayom_israeli_active))
+
+print((sum(item['pos_score'] for item in hayom_palestinian_passive) - (sum(item['neg_score'] for item in hayom_palestinian_passive)))/len(hayom_palestinian_passive))
+print((sum(item['pos_score'] for item in hayom_palestinian_active) - (sum(item['neg_score'] for item in hayom_palestinian_active)))/len(hayom_palestinian_active))
+
+print((sum(item['pos_score'] for item in hayom_israeli_passive) - (sum(item['neg_score'] for item in hayom_israeli_passive)))/len(hayom_israeli_passive))
+print((sum(item['pos_score'] for item in hayom_israeli_active) - (sum(item['neg_score'] for item in hayom_israeli_active)))/len(hayom_israeli_active))
+
+print(sum(item['obj_score'] for item in hayom_palestinian_passive)/len(hayom_palestinian_passive))
+print(sum(item['obj_score'] for item in hayom_palestinian_active)/len(hayom_palestinian_active))
+print(sum(item['obj_score'] for item in hayom_israeli_passive)/len(hayom_israeli_passive))
+print(sum(item['obj_score'] for item in hayom_israeli_active)/len(hayom_israeli_active))
